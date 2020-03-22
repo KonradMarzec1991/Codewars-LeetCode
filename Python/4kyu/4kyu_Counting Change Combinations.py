662e82d7ce0754e57b3f2e566d718d1494e49ff8
KonradMarzec1991 <konrimarzec@gmail.com>
2020-03-22 18:59:43 +0100

    4kyu problems

---
+++ Python/4kyu/4kyu_Counting Change Combinations.py
@@ -0,0 +1,13 @@
+def count_change(money, coins):
+    combinations = [0]*(money+1)
+    combinations[0] = 1
+
+    for coin in coins:
+        for i in range(len(combinations)):
+            if i >= coin:
+                combinations[i] += combinations[i-coin]
+    return combinations[money]
+
+
+
+
---
+++ Python/4kyu/4kyu_Explosive Sum.py
@@ -0,0 +1,17 @@
+def exp_sum(n):
+    if n <= 3:
+        return n
+    matrix = [[0]*(n+1) for _ in range(n+1)]
+    for i in range(n+1):
+        matrix[i][0] = 1
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if i > j:
+                matrix[i][j] = matrix[i-1][j]
+            else:
+                val_up = matrix[i-1][j]
+                val_combination = matrix[i][j-i]
+                matrix[i][j] = val_up + val_combination
+    return matrix[n][n]
+
+
