027798328f90962cf02d019f74989ea4f58dec4a
KonradMarzec1991 <konrimarzec@gmail.com>
2020-04-06 20:14:57 +0200

    Twice linear

---
+++ Python/4kyu/4kyu_Twice linear.py
@@ -0,0 +1,18 @@
+def dbl_linear(n):
+    arr = [1]
+    x0, y0 = 0, 0
+
+    for i in range(n):
+        x, y = 2*arr[x0] + 1, 3*arr[y0] + 1
+        arr.append(min(x, y))
+        if min(x, y) == x:
+            x0 += 1
+        if min(x, y) == y:
+            y0 += 1
+    return arr[n]
+
+
+print(dbl_linear(10))
+print(dbl_linear(20))
+print(dbl_linear(30))
+print(dbl_linear(50))
