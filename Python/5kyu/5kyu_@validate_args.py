5f9376250beb55eacb93f17289823a32f61f9d1d
KonradMarzec1991 <konrimarzec@gmail.com>
2021-01-06 20:37:27 +0100

    @validate_args - II solution

--- Python/5kyu/5kyu_@validate_args.py
+++ Python/5kyu/5kyu_@validate_args.py
@@ -5,6 +5,7 @@ class InvalidArgument(Exception):
     pass
 
 
+# function decorator
 def validate_args(*types_):
 
     def decorator(fn):
@@ -15,3 +16,16 @@ def validate_args(*types_):
             return fn(*args)
         return inner
     return decorator
+
+
+# class decorator
+class validate_args_cls:
+    def __init__(self, *types_):
+        self.types_ = types_
+
+    def __call__(self, fn):
+        def inner(*args):
+            if not all(isinstance(item, self.types_) for item in args):
+                raise InvalidArgument
+            return fn(*args)
+        return inner
