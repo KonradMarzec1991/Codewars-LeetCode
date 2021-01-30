1020d1661ff91bcd693daf74aff74a0d12c0c2b0
KonradMarzec1991 <konrimarzec@gmail.com>
2021-01-30 22:05:04 +0100

    Add 5kyu solution

--- Python/5kyu/5kyu_Limited number of instances.py
+++ Python/5kyu/5kyu_Limited number of instances.py
@@ -1,44 +1,32 @@
-from collections import defaultdict
-
-
 def limiter(limit, unique, lookup):
-    print(limit, unique, lookup)
-    instances = defaultdict(list)
+    uniq = {}
     global_lookup = 0
 
     if lookup == 'LAST':
         global_lookup = -1
-    if lookup == 'RECENT':
+    else:
         global_lookup = -2
 
     def wrapper(cls):
-        name = cls.__name__
-        original__new__ = cls.__new__
-
-        def other__new__(cls, *args, **kwargs):
-            instance = original__new__(cls)
-            instance.__init__(*args, **kwargs)
-
-            for obj in instances[name]:
-                if obj.__dict__[unique] == instance.__dict__[unique]:
-                    return instances[name][global_lookup]
-
-            if len(instances[name]) == limit:
-                return instances[name][global_lookup]
-
-            instances[name].append(instance)
-            return instance
-
-        cls.__new__ = other__new__
-        return cls
+        def inner(*args, **kwargs):
+            obj = cls(*args, **kwargs)
+            attr = getattr(obj, unique)
+            if attr in uniq:
+                return uniq[list(uniq.keys())[global_lookup]]
+            if len(uniq) == limit:
+                return uniq[list(uniq.keys())[global_lookup]]
+            uniq[attr] = obj
+            return obj
+        return inner
     return wrapper
 
 
 @limiter(2, "ID", "FIRST")
 class ExampleClass:
-    def __init__(self, ID, value):
-        self.ID, self.value = ID, value
-        print(self)
+    def __init__(self, ID, value): self.ID, self.value = ID, value
+
+    def __str__(self):
+        return f'{self.__class__.__name__}({self.ID},{self.value})'
 
 
 a = ExampleClass(1, 5)  # unique ID=1
