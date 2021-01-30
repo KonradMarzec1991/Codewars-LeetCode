6b071d79f20a5ed34ce95d56fc55b4c319ac0664
KonradMarzec1991 <konrimarzec@gmail.com>
2021-01-30 16:17:17 +0100

    Format 6kyu_Message_Validator.py file

--- Python/6kyu/6kyu_Message Validator.py
+++ Python/6kyu/6kyu_Message Validator.py
@@ -2,6 +2,10 @@ import re
 
 
 def is_a_valid_message(message):
+    if not message:
+        return True
+    if message[0].isalpha():
+        return False
     sub, num, char = [], '', ''
     for c in message:
         if c.isdigit():
@@ -14,11 +18,18 @@ def is_a_valid_message(message):
                 sub.append(int(num))
                 num = ''
             char += c
-    sub.append(char)
-    return all(sub[i] == len(sub[i+1]) for i in range(0, len(sub), 2)) \
-        if len(sub) % 2 == 0 else False
+    if char:
+        sub.append(char)
+    if num:
+        sub.append(num)
+    if len(sub) % 2 != 0:
+        return False
+    return all(sub[i] == len(sub[i+1]) for i in range(0, len(sub), 2))
 
 
 # Great CW solution
 def is_a_valid_message_2(message):
-    return all(int(m.group(1) or 0) == len(m.group(2)) for m in re.finditer(r'(\d*)([a-zA-Z]*)', message))
+    return all(
+        int(m.group(1) or 0) == len(m.group(2))
+        for m in re.finditer(r'(\d*)([a-zA-Z]*)', message)
+    )
