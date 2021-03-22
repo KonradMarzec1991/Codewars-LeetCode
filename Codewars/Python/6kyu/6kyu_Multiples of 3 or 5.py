818f3ccc58c0cf1c3d0407ce01c9ac23b67aff6e
KonradMarzec1991 <konrimarzec@gmail.com>
2021-03-22 22:28:51 +0100

    Multiples of 3 or 5

--- Codewars/Python/6kyu/6kyu_Multiples of 3 or 5.py
+++ Codewars/Python/6kyu/6kyu_Multiples of 3 or 5.py
@@ -1,6 +1,7 @@
 def solution(number):
-    sum = 0
-    for x in range(number):
-        if x % 3 == 0 or x % 5 == 0:
-            sum += x
-    return sum
+    i, count = 2, 0
+    while i < number:
+        if i % 3 == 0 or (i % 5 == 0 and i % 3 != 0):
+            count += i
+        i += 1
+    return count
