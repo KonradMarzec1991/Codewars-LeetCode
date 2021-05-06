a3a1de594e3a45a8793753a335bfa8fb3b5bb2a2
KonradMarzec1991 <konrimarzec@gmail.com>
2021-05-06 22:41:45 +0200

    Rewrite Tribonacci Sequence

--- Codewars/Python/6kyu/6kyu_Tribonacci Sequence.py
+++ Codewars/Python/6kyu/6kyu_Tribonacci Sequence.py
@@ -1,26 +1,17 @@
 def tribonacci(signature, n):
-
     if n == 0:
         return []
-
     if n <= 3:
         return signature[:n]
-
     a = signature[0]
     b = signature[1]
     c = signature[2]
 
-    n_array = []
-
-    n_array.append(a)
-    n_array.append(b)
-    n_array.append(c)
+    n_array = [a, b, c]
 
     i = 3
     while i < n:
-
         val = sum(n_array[-3:])
         n_array.append(val)
         i += 1
-
     return n_array
